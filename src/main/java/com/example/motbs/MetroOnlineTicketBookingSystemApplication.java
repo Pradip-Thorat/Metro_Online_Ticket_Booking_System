910619ea14f0eada5b55d4d57f59b38173de4fd1
Pradip-Thorat <pradipthorat0217@gmail.com>
2025-07-31 17:48:26 +0530

    New Project Starts on 31-July-25

---
+++ src/main/java/com/example/motbs/MetroOnlineTicketBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.motbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroOnlineTicketBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroOnlineTicketBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/motbs/MetroOnlineTicketBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.motbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroOnlineTicketBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
